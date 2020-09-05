676ffb4f86280a0f41513c4e1158a897634ab45a
Ovyerus <iamovyerus@gmail.com>
2020-09-05 10:53:27 +1000

    Log headers

--- api/up/index.ts
+++ api/up/index.ts
@@ -2,5 +2,6 @@ import { NowRequest, NowResponse } from "@vercel/node";
 
 export default (req: NowRequest, res: NowResponse) => {
   console.log(req.body);
+  console.log(req.headers);
   res.status(200).send("OK");
 };
