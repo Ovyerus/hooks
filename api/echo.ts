bca6ef7f7fa092050e3951040aeb0551c80a5966
Ovyerus <iamovyerus@gmail.com>
2020-09-05 10:06:37 +1000

    Add method to echo

--- api/echo.ts
+++ api/echo.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
-export default ({ body, query }: NowRequest, res: NowResponse) => {
-  res.json({ status: 200, body, query });
+export default ({ body, query, method }: NowRequest, res: NowResponse) => {
+  res.json({ status: 200, body, query, method });
 };
