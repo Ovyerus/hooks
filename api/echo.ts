885e1325693d0ab7e3d744e8cfa970918d0d66b9
Ovyerus <iamovyerus@gmail.com>
2020-09-05 10:26:30 +1000

    More echo stuff

--- api/echo.ts
+++ api/echo.ts
@@ -1,5 +1,25 @@
+import { text } from "micro";
+
 import { NowRequest, NowResponse } from "@vercel/node";
 
-export default ({ body, query, method }: NowRequest, res: NowResponse) => {
+const tryFn = (fn) => {
+  try {
+    return fn();
+  } catch {
+    return null;
+  }
+};
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const rawBody = await text(req);
+  const body = await tryFn(() => JSON.parse(rawBody));
+  const { query, method } = req;
+
   res.json({ status: 200, body, query, method });
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
