3be80b7b3ba1781591bb19102e6c78c816a563f3
Ovyerus <iamovyerus@gmail.com>
2020-09-05 10:32:44 +1000

    Add option for formatting echo resp

--- api/echo.ts
+++ api/echo.ts
@@ -14,8 +14,20 @@ export default async (req: NowRequest, res: NowResponse) => {
   const rawBody = await text(req);
   const body = await tryFn(() => JSON.parse(rawBody));
   const { query, method } = req;
+  const resp = { status: 200, body, rawBody, query, method };
 
-  res.json({ status: 200, body, query, method });
+  if (req.query.formatted == null)
+    res.json({ status: 200, body, rawBody, query, method });
+  else {
+    const fmtResp = JSON.stringify(
+      { status: 200, body, rawBody, query, method },
+      null,
+      4
+    );
+
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(fmtResp);
+  }
 };
 
 export const config = {
