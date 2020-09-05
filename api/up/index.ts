4a2c0e215969e618c3a0ad34cb49679cb9192e60
Ovyerus <iamovyerus@gmail.com>
2020-09-05 12:01:06 +1000

    Add secure thingy

--- api/up/index.ts
+++ api/up/index.ts
@@ -1,12 +1,16 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+import { json } from "micro";
 
 import { isSecureReq } from "../../util/up";
 
 export default async (req: NowRequest, res: NowResponse) => {
   if (req.method === "GET") return res.status(200).send("OK");
 
-  const secured = await isSecureReq(req);
-  console.log(secured);
+  if (!(await isSecureReq(req)))
+    return res.status(403).json({ status: 403, message: "Forbidden" });
+
+  // const body = await json(req);
+  res.send("OK");
   // console.log(req.body);
   // console.log(req.headers);
 };
